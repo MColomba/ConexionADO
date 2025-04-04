658e34ad0c1646c0e11cb8c4de569eb6f880a8e8
Alumno <Alumno@PC173>
2025-04-04 20:24:28 -0300

    Agregar archivos de proyecto.

---
+++ pryColombaConexionBD/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryColombaConexionBD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                clsConexionBD c = new clsConexionBD();
+
+                string query ="SELECT Nombre FROM Productos";
+              
+                    SqlCommand command = new SqlCommand(query,c.connection);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MessageBox.Show(reader["Nombre"].ToString());
+                        }
+                    }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+    }
+}
---
+++ pryColombaConexionBD/clsConexionBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryColombaConexionBD
+{
+    internal class clsConexionBD
+    {
+        public string connectionString = "Server=localhost;Database=Comercio;Trusted_Connection=True;";
+        public SqlConnection connection { get; set; }
+
+        public clsConexionBD()
+        {
+            connection = new SqlConnection(connectionString);
+            
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("✅ Conexión exitosa a la base de datos.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("❌ Error al conectar: " + ex.Message);
+                }
+            
+        }
+    }
+}
